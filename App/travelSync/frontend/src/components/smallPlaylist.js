8687fa1733a9fb73738148922bf53d2fe8ca9be0
domesimbana <domesimbana123@gmail.com>
2023-12-04 19:02:02 -0500

    Gradient of songs

--- App/travelSync/frontend/src/components/smallPlaylist.js
+++ App/travelSync/frontend/src/components/smallPlaylist.js
@@ -2,14 +2,8 @@ import React, { Component } from "react";
 import Button from "@material-ui/core/Button";
 import Grid from "@material-ui/core/Grid";
 import Typography from "@material-ui/core/Typography";
-import TextField from "@material-ui/core/TextField";
-import FormHelperText from "@material-ui/core/FormHelperText";
-import FormControl from "@material-ui/core/FormControl";
+import Paper from "@material-ui/core/Paper";
 import {Link} from "react-router-dom";
-import Radio from "@material-ui/core/Radio";
-import RadioGroup from "@material-ui/core/RadioGroup";
-import FormControlLabel from "@material-ui/core/FormControlLabel";
-
 
 export default class SmallPlaylist extends Component{
 
@@ -29,19 +23,23 @@ export default class SmallPlaylist extends Component{
                 <Typography component="subtitle1" variant="subtitle1">{user.name}'s Personalized Playlist</Typography>
                 <Typography component="body1" variant="body1">
                 {playlist && (
-                    <>
-                        <p>Playlist size: {playlist.length}</p>
-                        <ul>
-                            {playlist.map((song, index) => (
-                                <li key={index} style={{ background: "#ffffff" }}>
-                                    <Link to={song.uri}>
-                                        <p>{index + 1}: {song.name} by {song.artist}</p>
-                                    </Link>
-                                </li>
-                            ))}
-                        </ul>
-                    </>
-                )}
+    <>
+        <p>Playlist size: {playlist.length}</p>
+        <Grid container spacing={2}>
+            {playlist.map((song, index) => (
+                <Grid item key={index} xs={12}>
+                    <Paper style={{ padding: 10, background: "#ffffff" }}>
+                        <Link to={song.uri}>
+                            <Typography variant="body1">
+                                {index + 1}: {song.name} by {song.artist}
+                            </Typography>
+                        </Link>
+                    </Paper>
+                </Grid>
+            ))}
+        </Grid>
+    </>
+)}
                 </Typography>
                 <Button color="primary" variant="contained" to="/" component={Link}>Back</Button>
             </Grid>
